ffbf39c83a663de0e1443f51c984e75f43e53731
Liubavaa <92572643+Liubavaa@users.noreply.github.com>
2022-02-19 00:20:26 +0200

    Update tweep.py

--- task3/tweep.py
+++ task3/tweep.py
@@ -1,7 +1,13 @@
+"""
+Module use API and return information about user friend
+"""
 import tweepy
 
 
 def friend_info(name):
+    """
+    Return usernames and locations of friends of inputted user
+    """
     auth = tweepy.OAuthHandler("TM3E73Zu6QgpoY9xctnNNmVtb",
                                "O41Hsxoo2hpwTo84ODzTE0HtP8JdqRNN76Ehs9LL4UJmH7ko5i")
     auth.set_access_token("1492029335435419654-o8ExDUpguupbc34NX8G7QZ9R9Z8UJw",

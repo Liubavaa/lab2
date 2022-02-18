d56f0e1b3a1bc5dfdb6c3d664c8bb71c17d5ab97
Liubavaa <92572643+Liubavaa@users.noreply.github.com>
2022-02-19 00:19:29 +0200

    Update flask_app.py

--- task3/flask_app.py
+++ task3/flask_app.py
@@ -1,3 +1,6 @@
+"""
+Main web module
+"""
 from flask import Flask, render_template, request
 import map_make
 import tweep
@@ -8,11 +11,17 @@ app = Flask(__name__, static_folder="templates")
 
 @app.route("/")
 def index():
+    """
+    First page where user inputs username
+    """
     return render_template('index.html')
 
 
 @app.route("/map", methods=["POST"])
 def map_m():
+    """
+    Open page with map with user friend
+    """
     friend_info = tweep.friend_info(request.form.get("user_name"))
     if friend_info is None:
         return render_template('index.html')
